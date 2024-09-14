ccc350b9469c7215cd4bd9fa86dca237c3e00419
DorianKwan <bryce.sayers-kwan@live.ca>
2024-09-14 20:59:57 +0100

    refactor: heading component

--- src/app/components/common/Heading/Heading.tsx
+++ src/app/components/common/Heading/Heading.tsx
@@ -6,6 +6,7 @@ type HeadingType = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 export interface HeadingProps {
   readonly type?: HeadingType;
   readonly className?: string;
+  readonly fontSize?: React.CSSProperties['fontSize'];
   readonly children: React.ReactNode;
 }
 
@@ -32,16 +33,30 @@ export const Heading: React.FC<HeadingProps> = ({
   }
 };
 
-interface HeadingTextProps {}
+interface HeadingTextProps {
+  readonly fontSize?: React.CSSProperties['fontSize'];
+}
 
-const H1 = styled.h1<HeadingTextProps>``;
+const H1 = styled.h1<HeadingTextProps>`
+  font-size: ${({ fontSize }) => fontSize ?? '5rem'};
+`;
 
-const H2 = styled.h2<HeadingTextProps>``;
+const H2 = styled.h2<HeadingTextProps>`
+  font-size: ${({ fontSize }) => fontSize ?? '2.5rem'};
+`;
 
-const H3 = styled.h3<HeadingTextProps>``;
+const H3 = styled.h3<HeadingTextProps>`
+  font-size: ${({ fontSize }) => fontSize ?? '1.5rem'};
+`;
 
-const H4 = styled.h4<HeadingTextProps>``;
+const H4 = styled.h4<HeadingTextProps>`
+  font-size: ${({ fontSize }) => fontSize ?? '1.25rem'};
+`;
 
-const H5 = styled.h5<HeadingTextProps>``;
+const H5 = styled.h5<HeadingTextProps>`
+  font-size: ${({ fontSize }) => fontSize ?? '1rem'};
+`;
 
-const H6 = styled.h6<HeadingTextProps>``;
+const H6 = styled.h6<HeadingTextProps>`
+  font-size: ${({ fontSize }) => fontSize ?? '1rem'};
+`;
