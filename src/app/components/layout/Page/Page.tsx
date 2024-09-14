c53ae415584ff194cb83d1a9c00d57e7196cebee
DorianKwan <bryce.sayers-kwan@live.ca>
2024-09-14 21:00:22 +0100

    refactor: set page height to 100%

--- src/app/components/layout/Page/Page.tsx
+++ src/app/components/layout/Page/Page.tsx
@@ -4,4 +4,6 @@ export const Page: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return <StyledPage>{children}</StyledPage>;
 };
 
-const StyledPage = styled.div``;
+const StyledPage = styled.div`
+  height: 100%;
+`;
