f5b5c4cf10a34833c0136e8f6c80f2f3d70d20b6
DorianKwan <bryce.sayers-kwan@live.ca>
2024-09-15 19:38:29 +0100

    feat: add contact page content

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,18 +1,25 @@
-import styled from '@emotion/styled';
-import { Heading, LinkButton, Page } from '~/app/components';
-import { hexToRGBA, theme } from '~/theme';
+import {
+  Heading,
+  LinkButton,
+  Page,
+  CTAContainer,
+  PageContainer,
+  PageContentWrapper,
+  PageText,
+  PageTextHighlight,
+} from '~/app/components';
 
 const Contact: React.FC = () => {
   return (
     <Page>
-      <ContactContainer>
-        <ContactWrapper>
+      <PageContainer>
+        <PageContentWrapper maxWidth="85ch">
           <Heading>Let's Connect</Heading>
-          <ConnectText>
+          <PageText>
             If you have a project idea, team that needs developer assistance or
             believe I might be a good fit for a role you're hiring for
-            <CTAText>Get in touch with me!</CTAText>
-          </ConnectText>
+            <PageTextHighlight>Get in touch with me!</PageTextHighlight>
+          </PageText>
           <CTAContainer>
             <LinkButton
               href="mailto:bsayerskwan@gmail.com"
@@ -21,53 +28,10 @@ const Contact: React.FC = () => {
               Say hi <span>👋</span>
             </LinkButton>
           </CTAContainer>
-        </ContactWrapper>
-      </ContactContainer>
+        </PageContentWrapper>
+      </PageContainer>
     </Page>
   );
 };
 
 export default Contact;
-
-const ContactContainer = styled.section`
-  display: grid;
-  place-items: center;
-  height: 100%;
-`;
-
-const ContactWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 0.75rem;
-  max-width: 85ch;
-  margin-top: -2rem;
-  text-align: center;
-
-  > * {
-    width: fit-content;
-  }
-`;
-
-const ConnectText = styled.p`
-  font-size: 1.25rem;
-  color: ${({ theme }) => theme.colors.copyText};
-  margin-bottom: 1rem;
-`;
-
-const CTAText = styled.span`
-  margin-top: 0.75rem;
-  display: block;
-  color: ${({ theme }) => hexToRGBA(theme.colors.white, 0.6)};
-`;
-
-const CTAContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-  width: 100%;
-
-  @media only screen and (min-width: ${theme.breakpoints.md}) {
-    flex-direction: row;
-  }
-`;
