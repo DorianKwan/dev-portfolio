22abf574674a31f964c62ef038d1facc3810a022
DorianKwan <bryce.sayers-kwan@live.ca>
2026-04-09 15:54:11 -0600

    fix: emotion theme type inference

--- src/types/emotion.d.ts
+++ src/types/emotion.d.ts
@@ -1,51 +1,6 @@
-import { ThemeType } from '~/theme';
-import { Theme } from './theme';
+import { ThemeType } from '~/theme/theme';
 
 declare module '@emotion/react' {
-  export interface Theme extends ThemeType {
-    colors: {
-      background: string;
-      stringText: string;
-      highlight: string;
-      text: string;
-      darkText: string;
-      errorText: string;
-      darkBlue: string;
-      lightBlue: string;
-      bluePurple: string;
-      darkPurple: string;
-      lightPurple: string;
-      white: string;
-      black: string;
-      availabilityBg: string;
-      availabilityBorder: string;
-      copyrightText: string;
-      statusAvailable: string;
-      statusNotAvailable: string;
-      statusOpenToWork: string;
-      copyText: string;
-      aboutCardBg: string;
-      experienceOne: string;
-      experienceTwo: string;
-      experienceThree: string;
-      experienceFour: string;
-    };
-    breakpoints: {
-      xxs: string;
-      xs: string;
-      sm: string;
-      md: string;
-      xmd: string;
-      lg: string;
-      xl: string;
-      xxl: string;
-      xxxl: string;
-    };
-    layoutSpacing: {
-      md: string;
-      lg: string;
-      xxl: string;
-      xxxl: string;
-    };
-  }
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  export interface Theme extends ThemeType {}
 }
