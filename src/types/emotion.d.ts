8e2ae786f9d020d2c69a0e95949e59c1ae0d2f08
DorianKwan <bryce.sayers-kwan@live.ca>
2026-04-09 13:32:04 -0600

    fix: add emotion theme typing

--- src/types/emotion.d.ts
+++ src/types/emotion.d.ts
@@ -2,5 +2,50 @@ import { ThemeType } from '~/theme';
 import { Theme } from './theme';
 
 declare module '@emotion/react' {
-  export interface Theme extends ThemeType {}
+  export interface Theme extends ThemeType {
+    colors: {
+      background: string;
+      stringText: string;
+      highlight: string;
+      text: string;
+      darkText: string;
+      errorText: string;
+      darkBlue: string;
+      lightBlue: string;
+      bluePurple: string;
+      darkPurple: string;
+      lightPurple: string;
+      white: string;
+      black: string;
+      availabilityBg: string;
+      availabilityBorder: string;
+      copyrightText: string;
+      statusAvailable: string;
+      statusNotAvailable: string;
+      statusOpenToWork: string;
+      copyText: string;
+      aboutCardBg: string;
+      experienceOne: string;
+      experienceTwo: string;
+      experienceThree: string;
+      experienceFour: string;
+    };
+    breakpoints: {
+      xxs: string;
+      xs: string;
+      sm: string;
+      md: string;
+      xmd: string;
+      lg: string;
+      xl: string;
+      xxl: string;
+      xxxl: string;
+    };
+    layoutSpacing: {
+      md: string;
+      lg: string;
+      xxl: string;
+      xxxl: string;
+    };
+  }
 }
